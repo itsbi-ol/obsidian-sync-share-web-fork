08bda0d0ce313335e4381261efc73469ac48e7d8
Thanh Thai <thanhthai@fulygon.com>
2024-05-20 16:47:31 +0700

    update: github information
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- github.js
+++ github.js
@@ -1,6 +1,6 @@
-export const webSourceCodeUrl = "https://github.com/Alt-er/obsidian-sync-share";
+export const webSourceCodeUrl = "https://github.com/FuLygon/obsidian-sync-share-web";
 
-export const serverSourceCodeUrl = "https://github.com/Alt-er/obsidian-sync-share";
+export const serverSourceCodeUrl = "https://github.com/FuLygon/obsidian-sync-share-server";
 
 export const pluginSourceCodeUrl = "https://github.com/Alt-er/obsidian-sync-share";
 
